c878e9c580e06603c7bb9753233d0e123fe3c20f
MichiMauch <michi.mauch@gmail.com>
2024-05-04 06:20:28 +0200

    test

--- pages/api/solar.js
+++ pages/api/solar.js
@@ -2,11 +2,10 @@
 import axios from 'axios';
 
 export default async function handler(req, res) {
-  const url = 'https://vrmapi.victronenergy.com/v2/installations/193415/stats?interval=15mins';
+  const url = 'https://vrmapi.victronenergy.com/v2/installations/193415/stats?interval=15mins&type=live_feed';
   const config = {
     headers: { 'x-authorization': `Bearer ${process.env.BEARER_TOKEN}` }
   };
-
   try {
     const response = await axios.get(url, config);
     console.log("API Response:", response.data); // Logge die API-Antwort
